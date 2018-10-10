3b50fed3eeaa64584283894bc212a4565d438df2
Sheik <Sheik@IBM158-R9019EAM.in.ibm.com>
2018-10-10 15:47:50 +0530

    Version0.99 ,made the changes for the drop down and changed code for till 48 statements .

--- Next Statement Calculator/src/NCEstatementDateCalculatorApplet/StatementDateCalculatorAppletVersion096.java
+++ Next Statement Calculator/src/NCEstatementDateCalculatorApplet/StatementDateCalculatorAppletVersion096.java
@@ -105,7 +105,7 @@ public class StatementDateCalculatorAppletVersion096 {
 		comboBox_2.setBounds(414, 99, 59, 20);
 		frmNceStatementForecast.getContentPane().add(comboBox_2);
 		JComboBox comboBox_3 = new JComboBox();
-		comboBox_3.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12","13","14","15","16","17","18","19","20"}));
+		comboBox_3.setModel(new DefaultComboBoxModel(new String[] {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48"}));
 		comboBox_3.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(199, 21, 133), new Color(255, 255, 0), new Color(0, 0, 205), new Color(255, 0, 0)));
 		comboBox_3.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		comboBox_3.setForeground(new Color(0, 0, 0));
